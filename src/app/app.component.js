278be887917f3c04e5c4480a95322e4d7aed919a
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-22 14:11:14 -0200

    Listando Dados

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,7 +1,44 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var angularfire2_1 = require('angularfire2');
+require('../../public/css/styles.css');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(af) {
+        this.af = af;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.items = this.af.database.list('/courses', {
+            query: {
+                orderByChild: 'votes'
+            }
+        }).map(function (items) {
+            items.sort(function (a, b) {
+                return a.votes > b.votes;
+            });
+            return items.reverse();
+        });
+        this.af.auth.subscribe(function (user) {
+            _this.user = user;
+        });
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            templateUrl: './app.component.html',
+            styleUrls: ['./app.component.css']
+        }), 
+        __metadata('design:paramtypes', [angularfire2_1.AngularFire])
+    ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,34 @@
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
 export class AppComponent {
-  constructor() {}
+  items: FirebaseListObservable<any[]>;
+  user: any;
+
+  constructor(private af: AngularFire) {}
+
+  ngOnInit() {
+    this.items = this.af.database.list('/courses', {
+      query: {
+        orderByChild: 'votes'
+      }
+    }).map(items => {
+      items.sort((a: any, b: any) => {
+        return a.votes > b.votes;
+      });
+      return items.reverse();
+    }) as FirebaseListObservable<any[]>;
+
+    this.af.auth.subscribe(user => {
+      this.user = user;
+    });
+  }
 }
